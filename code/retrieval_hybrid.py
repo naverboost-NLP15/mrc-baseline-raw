2acf2598a91c66a55ac4c25712d5df3e6418ff54
lim010111 <shinedngus225@gmail.com>
2025-12-05 09:23:57 +0900

    refactor: Refactor langchain based hybird retriever(__init__, get_embedding)

--- code/retrieval_hybrid.py
+++ code/retrieval_hybrid.py
@@ -2,6 +2,7 @@ import os
 import json
 import time
 import pickle
+from turtle import title
 import pandas as pd
 import numpy as np
 from tqdm.auto import tqdm
@@ -26,19 +27,18 @@ def timer(name):
 class HybridRetrieval:
     def __init__(
         self,
-        tokenize_fn: Callable,
-        data_path: str | None = "./raw/data",
-        context_path: str | None = "wikipedia_documents.json",
+        tokenize_fn=None,
+        data_path: str = "./raw/data",
+        context_path: str = "wikipedia_documents.json",
     ) -> None:
         """
         BM25(Sparse) + FAISS(Dense)를 결합한 Hybrid Retriever
 
         Args:
-            tokenize_fn (Callable): _description_
+            tokenize_fn (None): Defaults to Kiwi tokenizer
             data_path (str | None, optional): _description_. Defaults to "./raw/data".
             context_path (str | None, optional): _description_. Defaults to "wikipedia_documents.json".
         """
-        self.tokenize_fn = tokenize_fn
         self.data_path = data_path
         self.context_path = context_path
 
@@ -46,106 +46,106 @@ class HybridRetrieval:
         with open(os.path.join(data_path, context_path), "r", encoding="utf-8") as f:
             wiki: dict = json.load(f)
 
-        # 중복 제거 및 Langchain Document 형식으로 변환
-        # text + title 정보 추가, 중복 제거는 text 기준으로만 변경
+        self.texts = []
+        self.titles = []
+        self.ids = []
 
-        self.documents = []
-        seen_texts = set()
-
-        print("document 처리 중 (Title + Text 전략)...")
+        # 중복 제거 및 데이터 리스트 생성
+        print("중복 text 제거 중...")
+        seen_texts = set()  # 모든 id값은 고유하지만, 중복되는 text가 존재하므로, 제거
         for v in wiki.values():
-            text = v["text"]
-            title = v["title"]
-            doc_id = v["document_id"]
+            text, title, id = v["text"], v["title"], v["document_id"]
+            if text not in seen_texts:
+                self.texts.append(text)
+                self.titles.append(title)
+                self.ids.append(id)
+                seen_texts.add(text)
 
-            if text in seen_texts:
-                continue
+        # Kiwi 형태소 분석기
+        self.kiwi = Kiwi()
+        # Dense Model Load
+        self.embedding_model_name = (
+            "Qwen/Qwen3-Embedding-0.6B"  # TODO: Fix Embedding Model
+        )
+        self.encoder = SentenceTransformer(self.embedding_model_name)
 
-            seen_texts.add(text)
+        self.bm25 = None
+        self.faiss_index = None
 
-            combined_context = f"{title}\n{text}"
+    # TODO: Ensemble Retriever 구현
 
-            self.documents.append(
-                Document(
-                    page_content=combined_context,
-                    metadata={
-                        "id": doc_id,
-                        "title": title,
-                        "original_text": text,
-                    },
-                )
-            )
+    def kiwi_tokenizer(self, text: str) -> list[str]:
+        """
+        형태소의 품사가 명사, 동사, 형용사 등인 형태소만 추출
 
-        # Retriever 초기화
-        self.retriever: EnsembleRetriever | None = None
+        Args:
+            text (str): 원본 text
+
+        Returns:
+            list[str]: 추출된 형태소 Tokens
+        """
+        tokens = self.kiwi.tokenize(text)
+        return [
+            t.form for t in tokens if t.tag.startswith("N") or t.tag.startswith("V")
+        ]
 
     def get_embedding(self) -> None:
         """
-        BM25 Retriever와 Dense Retriever를 생성하거나 로드하여 Ensemble Retriever를 구축합니다.
+        BM25 Retriever와 Dense Retriever를 생성하거나 로드하여 Ensemble Retriever를 구축
         """
 
         # sparse, dense embedding 저장 경로 설정
-        bm25_path = os.path.join(self.data_path, "bm25_retriever.pkl")
-        faiss_path = os.path.join(self.data_path, "faiss_index")
+        embedding_model_name_splitted = self.embedding_model_name.split("/")[1]
 
-        # Dense 설정
-        # TODO: 한국어 성능이 좋은 모델 선정...
-        model_name = "Qwen/Qwen3-Embedding-0.6B"
-        model_kwargs = {"device": "cuda" if torch.cuda.is_available() else "cpu"}
-        encode_kwargs = {"normalize_embeddings": True}
-
-        hf = HuggingFaceEmbeddings(
-            model_name=model_name,
-            model_kwargs=model_kwargs,
-            encode_kwargs=encode_kwargs,
+        bm25_path = os.path.join(self.data_path, "bm25_wiki.pkl")
+        faiss_path = os.path.join(
+            self.data_path, f"faiss_{embedding_model_name_splitted}.index"
         )
 
-        if os.path.exists(faiss_path):
-            print("FAISS index 로딩 중...")
-            vectorstore = FAISS.load_local(
-                folder_path=faiss_path,
-                embeddings=hf,
-                allow_dangerous_deserialization=True,
-            )
-        else:
-            print("FAISS index 구축 중...")
-            vectorstore = FAISS.from_documents(self.documents, hf)
-            vectorstore.save_local(folder_path=faiss_path)
-            print("FAISS index 저장이 완료되었습니다.")
-
-        faiss_retriever = vectorstore.as_retriever(search_kwargs={"k": 10})
-
-        # Sparse 설정
+        # Sparse(BM25) 설정
         if os.path.isfile(bm25_path):
             print("BM25 retriever 로딩 중...")
             with open(bm25_path, "rb") as f:
-                bm25_retriever = pickle.load(f)
+                self.bm25 = pickle.load(f)
 
         else:
             print("BM25 retriever 구축 중...")
-            # tokenize_fn을 사용하여 토큰화 후 BM25 생성
-            # LangChain BM25는 기본적으로 공백 split을 사용하므로,
-            # 한글 처리를 위해 preprocess_func를 커스텀하거나 토큰화된 리스트를 넘겨야 함.
-            # 여기서는 편의상 from_documents 사용 (내부적으로 기본 토크나이저 사용됨)
-            # TODO: 성능을 높이려면 konlpy Mecab 등의 토크나이저로 전처리가 필요함.
+            # text + title로 합쳐서 인덱싱합니다.
 
-            bm25_retriever = BM25Retriever.from_documents(
-                self.documents,
-                preprocess_func=self.tokenize_fn,
-            )
-            bm25_retriever.k = 10
+            tokenized_corpus = [
+                self.kiwi_tokenizer(tit + " " + txt)
+                for tit, txt in zip(self.titles, self.texts)
+            ]
+            self.bm25 = BM25Okapi(corpus=tokenized_corpus)
 
             with open(bm25_path, "wb") as f:
-                pickle.dump(bm25_retriever, f)
+                pickle.dump(self.bm25, f)
             print("BM25 retriever saved.")
+            # TODO: bm25_retriever.k = 10
 
-        # 3. Ensemble 설정
-        # weights: [BM25 가중치, Dense 가중치]. 보통 0.3:0.7 또는 0.5:0.5 사용
-        print("Ensemble Retriever 초기화 중...")
-        self.retriever = EnsembleRetriever(
-            retrievers=[bm25_retriever, faiss_retriever],
-            weights=[0.5, 0.5],
-        )
+        # Dense 설정
+
+        if os.path.exists(faiss_path):
+            print("FAISS index 로딩 중...")
+            self.faiss_index = faiss.read_index(faiss_path)
+        else:
+            print("FAISS index 구축 중...")
+            # vectorstore = FAISS.from_documents(self.documents, hf)
+            docs = [f"{tit}\n{txt}" for tit, txt in zip(self.titles, self.texts)]
+            embeddings = self.encoder.encode(
+                sentences=docs,
+                batch_size=32,
+                show_progress_bar=True,
+                normalize_embeddings=True,
+            )
+
+            dim = embeddings.shape[1]
+            self.faiss_index = faiss.IndexFlatIP(
+                dim
+            )  # Inner Product (Cosine Similarity because normalized)
+            self.faiss_index.add(embeddings)
+            faiss.write_index(self.faiss_index, faiss_path)
+            print("FAISS index 저장이 완료되었습니다.")
 
     def retrieve(
         self,
